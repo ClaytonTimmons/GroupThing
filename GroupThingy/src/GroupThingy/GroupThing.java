967c2fa8589edea250d68785b24c2ad1c651ab6e
ClaytonTimmons <60448885+ClaytonTimmons@users.noreply.github.com>
2020-02-06 14:16:42 -0500

    firts

---
+++ GroupThingy/src/GroupThingy/GroupThing.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GroupThingy;
+
+/**
+ *
+ * @author minab
+ */
+public class GroupThing {
+
+    public static void Help(){
+        System.out.println("Henlo, am girafe");
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Help();
+    }
+    
+}
